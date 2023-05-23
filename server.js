4e10d71bd077f283a5938a49f42f4e9d05f86d3a
Amir Javed <amirjaved@amirs-air.home>
2023-05-23 23:31:20 +0100

    Added Database Connection

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
 const productRoutes = require('./routes/productRoutes');
 
 const app = express();
+
+mongoose.connect(process.env.DB_LINK).then(() => console.log('DB Online')).catch(err => console.log(err));
 
 app.use('/api/products', productRoutes);
 
 app.get('/', (req, res, next) => res.json({message: "Server Online"}))
 
-app.listen(4000);
+app.listen(process.env.PORT);
